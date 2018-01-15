c6c34ffb495835c4f1f2d243e670586d03c59aef
Simo Mazil <mohamed.mazil@gmail.com>
2018-01-15 22:40:24 +0000

    Install gulp-sass & Add Gulp task to Compile Sass to Css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
+'use-strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-  // place code for your default task here
-  console.log("Work!!!");
+gulp.task('default', function(done) {
+  gulp.src('./sass/*.sass').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./css'));
   done();
-}
+});
