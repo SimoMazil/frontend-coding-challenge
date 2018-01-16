202a7fff36bd6b16a5ae5353573fec8822d430c2
Simo Mazil <mohamed.mazil@gmail.com>
2018-01-16 00:28:10 +0000

    Add Gulp Watch Sass, Install Browser-Sync, Add some Sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
-'use-strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function(done) {
-  gulp.src('./sass/*.sass').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./css'));
-  done();
+gulp.task('sass', function (done) {
+    gulp.src('sass/imports.sass')
+        .pipe(sass({includePaths: ['sass']}))
+        .pipe(gulp.dest('css'));
+        done();
 });
+
+gulp.task('browser-sync', function(done) {
+    browserSync.init(["css/*.css"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'browser-sync', function(done) {
+  gulp.watch("sass/*.sass", gulp.series("sass"));
+  done();
+}));
