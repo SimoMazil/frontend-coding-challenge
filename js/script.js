545978dac9e30a209a4f91bad5068787a2b23c1a
Simo Mazil <mohamed.mazil@gmail.com>
2018-01-17 12:53:53 +0000

    Add the icons to the cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
-  console.log("Jquery !!");
+    verticalAlignArrows()
 })
+
+$(window).on('resize', function(){
+    verticalAlignArrows()
+});
+
+function verticalAlignArrows() {
+  const slide_height = $(".slide img").height()
+  const arrow_dynamic_height = (slide_height - 30) / 2
+  $(".arrow-left, .arrow-right").css({"top": `${arrow_dynamic_height}px`})
+}
