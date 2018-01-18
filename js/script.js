58d333a4a0ab90d4a93230fa748633f83aafdbaf
Simo Mazil <mohamed.mazil@gmail.com>
2018-01-18 01:09:02 +0000

    Add SlideJS lib, Slide Cards Images working

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 $(function () {
-    verticalAlignArrows()
+  $(".images-slides").slidesjs({
+    navigation: {
+      active: false
+    },
+    pagination: {
+      active: false
+    }
+  });
+
+  verticalAlignArrows()
 })
 
 $(window).on('resize', function(){
-    verticalAlignArrows()
+  verticalAlignArrows()
 });
 
 function verticalAlignArrows() {
-  const slide_height = $(".slide img").height()
-  const arrow_dynamic_height = (slide_height - 30) / 2
-  $(".arrow-left, .arrow-right").css({"top": `${arrow_dynamic_height}px`})
+  setTimeout(function () {
+    const slide_height = $(".hover-events").height()
+    const arrow_dynamic_height = (slide_height - 30) / 2
+    $(".arrow-left, .arrow-right").css({"top": `${arrow_dynamic_height}px`})
+  }, 50)
 }
